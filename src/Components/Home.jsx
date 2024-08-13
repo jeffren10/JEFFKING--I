13c0c7948a3c14ff012270ded42a7ff02f15276d
jeffren10 <ijeffren1@gmail.com>
2024-08-13 20:23:30 +0100

    first commit done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'> View Work 
             <span className='group-hover:rotate-90 duration-300'> 
-            <HiArrowNarrowRight className='ml-3' />
+            <HiArrowNarrowRight className='ml-3' />    
               </span>
           </button>
           </div>  
