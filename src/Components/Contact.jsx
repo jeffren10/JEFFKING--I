9d9ed6630d39f63c827ef1f831d65065dd4602ef
jeffren10 <ijeffren1@gmail.com>
2024-08-15 00:46:56 +0100

    first commit romove overflow

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,17 +15,17 @@ const Contact = () => {
     form.reset();
   }
   return (
-    <div name='contact' className='w-full md:h-screen overflow-hidden bg-[#0a192f] flex justify-center items-center p-4 '>
+    <div name='contact' className='w-full md:h-screen  bg-[#0a192f] flex justify-center items-center p-4 '>
       <form onSubmit={onFinish} className='flex flex-col max-w-[600px] w-full'> 
-        <div className='pb-3'> 
+        <div className=''> 
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Contact </p>
-          <p className='text-gray-300 py-4'>// submit the form below or shoot me an email -ijeffren1@gmail.com </p>
+          <p className='text-gray-300 py-2'>// submit the form below or shoot me an email -ijeffren1@gmail.com </p>
         </div>
         <input htmlFor="name" className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name'id='name' />
         <input htmlFor="email" className='my-4 p-2 bg-[#ccd6f6] ' type='email' placeholder='Email'id='email' name='email' />
         <input htmlFor="subject" className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='subject' name='subject' id='subject'></input>
         <textarea htmlFor="message" className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='Message' ></textarea>
-        <button type='submit' className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 my-3 mx-auto flex items-center'  >Let's collaborate</button>
+        <button type='submit' className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 mx-auto flex items-center'  >Let's collaborate</button>
       </form>
     </div>
   )
